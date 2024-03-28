4be848f8e4a3836b730ddb1cee6e04b79035c6c3
Habarug <habarug@gmail.com>
2024-03-28 19:43:31 +0100

    get_EURO_matches working

--- EURObot/footballData/footballData.py
+++ EURObot/footballData/footballData.py
@@ -1,14 +1,59 @@
+from datetime import datetime as dt
+from datetime import timedelta as timedelta
+import difflib
 import requests
 import os
 import pyjson5
+
+# Resources for football-data API https://docs.football-data.org/general/v4/resources.html
 
 
 class FootballData:
 
     url_base = "https://api.football-data.org/v4/"
+    t_min = 60  # minimum minutes between same API call
 
     def __init__(self, KEY):
-        self.header = {"X-Auth-Token": self.key}
+        self.header = {"X-Auth-Token": KEY}
+
+        self.competitions_time = dt.min
+        self.EURO_time = dt.min
+
+    def get_competitions_list(self):
+        time = dt.now()
+        if ((time - self.competitions_time).total_seconds() / 60) > self.t_min:
+            try:
+                response = requests.get(
+                    self.url_base + "competitions", headers=self.header
+                )
+            except Exception as e:
+                raise e
+            else:
+                self.competitions_raw = response.json()
+                self.competitions_time = time
+
+            self.competitions = []
+            for comp in self.competitions_raw["competitions"]:
+                self.competitions.append(comp["name"])
+        return self.competitions
+
+    def get_EURO_matches(self, year=2024):
+        time = dt.now()
+
+        if ((time - self.EURO_time).total_seconds() / 60) > self.t_min:
+            try:
+                response = requests.get(
+                    self.url_base + "competitions/EC/matches?season=" + str(year),
+                    headers=self.header,
+                )
+            except Exception as e:
+                raise e
+            else:
+                self.data_raw = response.json()
+                self.EURO_matches = self.data_raw["matches"]
+                self.EURO_time = time
+
+        return self.EURO_matches
 
 
 def main():
@@ -17,6 +62,10 @@ def main():
     ) as f:
         priv = pyjson5.load(f)
 
-    FD = FootballData(priv["football_data"])
+    FD = FootballData(priv["footballData"])
 
     return FD
+
+
+if __name__ == "__main__":
+    main()
